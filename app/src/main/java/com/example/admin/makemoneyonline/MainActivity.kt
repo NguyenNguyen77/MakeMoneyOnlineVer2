08703ea9f3c0da00b7959f72e007bf915015afe9
KhoaNguyen8076 <ndkhoa17@gmail.com>
2019-01-02 23:53:59 +0700

    [KhoaNguyen] Remove table layout sample change to MainActivity

--- app/src/main/java/com/example/admin/makemoneyonline/MainActivity.kt
+++ app/src/main/java/com/example/admin/makemoneyonline/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.admin.makemoneyonline
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import com.example.admin.makemoneyonline.adapter.TabPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,12 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_log_in.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View): Unit {
-                // Handler code here.
-                startActivity(Intent(this@MainActivity,TablayoutDemo::class.java))
-            }
-        })
+
+        val fragmentAdapter = TabPagerAdapter(supportFragmentManager)
+        if(viewpager_main!=null) {
+            viewpager_main.adapter = fragmentAdapter
+            tab_layout.setupWithViewPager(viewpager_main)
+        }
 
     }
 
